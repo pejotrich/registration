9b161d68590a622f9b0fdaea1f5f4abd78cf4f19
Johanna Petrich <pejotrich@gmail.com>
2020-12-24 02:21:15 +0100

    all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "login", to: "pages#login"
+  get "registrierung", to: "pages#registrierung"
+  get "registrierung1", to: "pages#registrierung1"
+  get "registrierung2", to: "pages#registrierung2"
+  get "password", to: "pages#password"
+  get "address", to: "pages#address"
+  get "foto", to: "pages#foto"
 end
